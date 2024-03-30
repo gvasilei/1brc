c76ef809c3e825ebfc4a4db814abd93b629500a4
Giorgos Vasileiou <g.vasileiou@gmail.com>
2024-03-30 22:56:05 +0200

    Added a task queue to calculate min, max and averages concurrently. Not much gain

--- src/main/go/gvasilei/main.go
+++ src/main/go/gvasilei/main.go
@@ -7,10 +7,71 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime"
 	"runtime/trace"
 	"strconv"
 	"strings"
+	"sync"
 )
+
+type TemperatureStats struct {
+	mix float64
+	max float64
+	avg float64
+}
+
+type CityRecording struct {
+	city  string
+	temps []float64
+}
+
+type CityStats struct {
+	mu    sync.RWMutex
+	stats map[string]TemperatureStats
+}
+
+func NewCityStats() *CityStats {
+	return &CityStats{
+		stats: make(map[string]TemperatureStats),
+	}
+}
+
+// Set a key-value pair in the map safely.
+func (sm *CityStats) Set(key string, value TemperatureStats) {
+	sm.mu.Lock() // Acquire the lock for writing
+	sm.stats[key] = value
+	sm.mu.Unlock() // Release the lock after writing
+}
+
+// Get a value from the map safely.
+func (sm *CityStats) Get(key string) (TemperatureStats, bool) {
+	sm.mu.RLock() // Acquire the lock for reading
+	value, exists := sm.stats[key]
+	sm.mu.RUnlock() // Release the lock after reading
+	return value, exists
+}
+
+func worker(id int, recordings <-chan CityRecording, wg *sync.WaitGroup, stats *CityStats) {
+	defer wg.Done()
+	for recording := range recordings {
+		min := math.Inf(1)
+		max := math.Inf(-1)
+		sum := 0.0
+
+		for _, temp := range recording.temps {
+			sum += temp
+			if temp < min {
+				min = temp
+			}
+			if temp > max {
+				max = temp
+			}
+		}
+		avg := sum / float64(len(recording.temps))
+		stats.Set(recording.city, TemperatureStats{min, max, avg})
+		//fmt.Printf("Worker %d finished task\n", id)
+	}
+}
 
 var traceFile = flag.String("trace", "", "write trace execution to `file`")
 
@@ -38,15 +99,9 @@ func main() {
 	}
 	defer file.Close()
 
-	type TemperatureStats struct {
-		mix float64
-		max float64
-		avg float64
-	}
-
 	scanner := bufio.NewScanner(file)
 	measurements := make(map[string][]float64)
-	stats := make(map[string]TemperatureStats)
+	stats := NewCityStats()
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -62,24 +117,21 @@ func main() {
 
 	}
 
-	for city, temps := range measurements {
+	var wg sync.WaitGroup
+	recordingsChannel := make(chan CityRecording, 50)
 
-		min := math.Inf(1)
-		max := math.Inf(-1)
-		sum := 0.0
-
-		for _, temp := range temps {
-			sum += temp
-			if temp < min {
-				min = temp
-			}
-			if temp > max {
-				max = temp
-			}
-		}
-		avg := math.Round(sum / float64(len(temps)))
-		stats[city] = TemperatureStats{min, max, avg}
+	// Create Task Workers to distribute temperature stats calculation tasks
+	for i := 0; i < runtime.NumCPU()-1; i++ {
+		wg.Add(1)
+		go worker(i, recordingsChannel, &wg, stats)
 	}
+
+	for city, temps := range measurements {
+		recordingsChannel <- CityRecording{city, temps}
+	}
+
+	close(recordingsChannel) // No more tasks to be added, close the channel.
+	wg.Wait()
 
 	//for city := range stats {
 	//	fmt.Printf("City: %s, Min: %.2f, Max: %.2f, Avg: %.2f\n", city, stats[city].mix, stats[city].max, stats[city].avg)
