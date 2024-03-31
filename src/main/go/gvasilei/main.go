fd09c3c6f3f99f5fe1d5ab380fcb27963ca6f495
Giorgos Vasileiou <g.vasileiou@gmail.com>
2024-03-31 23:41:07 +0300

    Moved task workers at the initial processing. Don't keep all the recordings but rather just the min, max, sum and count. Also stopped using concurrent maps. Each worker returns his own map of recordings and they're all merged at the end.

--- src/main/go/gvasilei/main.go
+++ src/main/go/gvasilei/main.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"os"
 	"runtime"
+	"runtime/pprof"
 	"runtime/trace"
 	"strconv"
 	"strings"
@@ -15,65 +16,55 @@ import (
 )
 
 type TemperatureStats struct {
-	mix float64
-	max float64
-	avg float64
+	min   float64
+	max   float64
+	sum   float64
+	count int
+	avg   float64
 }
 
-type CityRecording struct {
-	city  string
-	temps []float64
+func (stats TemperatureStats) String() string {
+	return fmt.Sprintf("Min: %.2f, Max: %.2f, Avg: %.2f", stats.min, stats.max, stats.avg)
 }
 
-type CityStats struct {
-	mu    sync.RWMutex
-	stats map[string]TemperatureStats
-}
-
-func NewCityStats() *CityStats {
-	return &CityStats{
-		stats: make(map[string]TemperatureStats),
-	}
-}
-
-// Set a key-value pair in the map safely.
-func (sm *CityStats) Set(key string, value TemperatureStats) {
-	sm.mu.Lock() // Acquire the lock for writing
-	sm.stats[key] = value
-	sm.mu.Unlock() // Release the lock after writing
-}
-
-// Get a value from the map safely.
-func (sm *CityStats) Get(key string) (TemperatureStats, bool) {
-	sm.mu.RLock() // Acquire the lock for reading
-	value, exists := sm.stats[key]
-	sm.mu.RUnlock() // Release the lock after reading
-	return value, exists
-}
-
-func worker(id int, recordings <-chan CityRecording, wg *sync.WaitGroup, stats *CityStats) {
+func temperatureStatsWorker(id int, lines <-chan string, results chan<- map[string]TemperatureStats, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for recording := range recordings {
-		min := math.Inf(1)
-		max := math.Inf(-1)
-		sum := 0.0
 
-		for _, temp := range recording.temps {
-			sum += temp
-			if temp < min {
-				min = temp
-			}
-			if temp > max {
-				max = temp
-			}
+	stats := make(map[string]TemperatureStats)
+
+	for line := range lines {
+		parts := strings.Split(line, ";")
+		city := parts[0]
+		temperature, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
 		}
-		avg := sum / float64(len(recording.temps))
-		stats.Set(recording.city, TemperatureStats{min, max, avg})
+
+		recording, exists := stats[city]
+		if !exists {
+			stats[city] = TemperatureStats{math.Inf(1), math.Inf(-1), temperature, 1, 0.0}
+		} else {
+			if temperature < recording.min {
+				recording.min = temperature
+			} else if temperature > recording.max {
+				recording.max = temperature
+			}
+			recording.sum += temperature
+			recording.count++
+
+			stats[city] = recording
+		}
+
 		//fmt.Printf("Worker %d finished task\n", id)
 	}
+
+	results <- stats
 }
 
 var traceFile = flag.String("trace", "", "write trace execution to `file`")
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 	log.Println("Starting the application...")
@@ -92,6 +83,34 @@ func main() {
 		defer trace.Stop()
 	}
 
+	if *cpuprofile != "" {
+		f, err := os.Create("./profiles/" + *cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	calculate()
+
+	if *memprofile != "" {
+		f, err := os.Create("./profiles/" + *memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+}
+
+func calculate() {
 	file, err := os.Open("../../../../measurements.txt")
 	if err != nil {
 		fmt.Println("Error: ", err)
@@ -100,42 +119,54 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	measurements := make(map[string][]float64)
-	stats := NewCityStats()
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.Split(line, ";")
-		city := parts[0]
-		temperature, err := strconv.ParseFloat(parts[1], 64)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			return
-		}
-
-		measurements[city] = append(measurements[city], temperature)
-
-	}
+	stats := make(map[string]TemperatureStats)
 
 	var wg sync.WaitGroup
-	recordingsChannel := make(chan CityRecording, 50)
+	numWorkers := runtime.NumCPU() - 1
+	linesChannel := make(chan string, 1000)
+	results := make(chan map[string]TemperatureStats, numWorkers)
 
 	// Create Task Workers to distribute temperature stats calculation tasks
-	for i := 0; i < runtime.NumCPU()-1; i++ {
+	log.Printf("Adding %d workers to process temperature stats\n", numWorkers)
+	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
-		go worker(i, recordingsChannel, &wg, stats)
+		go temperatureStatsWorker(i, linesChannel, results, &wg)
 	}
 
-	for city, temps := range measurements {
-		recordingsChannel <- CityRecording{city, temps}
+	for scanner.Scan() {
+		linesChannel <- scanner.Text()
 	}
 
-	close(recordingsChannel) // No more tasks to be added, close the channel.
+	close(linesChannel) // No more tasks to be added, close the channel.
 	wg.Wait()
+	close(results)
 
-	//for city := range stats {
-	//	fmt.Printf("City: %s, Min: %.2f, Max: %.2f, Avg: %.2f\n", city, stats[city].mix, stats[city].max, stats[city].avg)
-	//}
+	log.Println("Merging results...")
+	for resultsMap := range results {
+		for city, recording := range resultsMap {
+			existingRecording, exists := stats[city]
+			if !exists {
+				stats[city] = recording
+			} else {
+				if recording.min < existingRecording.min {
+					existingRecording.min = recording.min
+				}
+				if recording.max > existingRecording.max {
+					existingRecording.max = recording.max
+				}
+				existingRecording.sum += recording.sum
+				existingRecording.count += recording.count
+				stats[city] = existingRecording
+			}
+		}
+	}
+
+	log.Println("Calculating averages...")
+	for city, recording := range stats {
+		recording.avg = recording.sum / float64(recording.count)
+		stats[city] = recording
+	}
 	log.Println("Outputting stats...")
+
 	fmt.Print(stats)
 }
