f64bd49f6144b2f2befe28429fdb0af4d2335e65
Giorgos Vasileiou <g.vasileiou@gmail.com>
2024-04-04 17:09:54 +0300

    Small improvements around map creation. Also created three variants of data loading. Memory Mapped, with multiple scanners and with a single buffer.

--- calculate_average_gvasilei.sh
+++ calculate_average_gvasilei.sh
@@ -15,5 +15,5 @@
 #  limitations under the License.
 #
 
-JAVA_OPTS=""
-java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_gvasilei
+INPUT=${1:-"measurements.txt"}
+src/main/go/gvasilei/gvasilei -measurements=$INPUT
--- src/main/go/gvasilei/main.go
+++ src/main/go/gvasilei/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"runtime"
 	"runtime/pprof"
@@ -13,7 +13,13 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
 )
+
+type Chunk struct {
+	start int64
+	end   int64
+}
 
 type TemperatureStats struct {
 	min   float64
@@ -27,100 +33,20 @@ func (stats TemperatureStats) String() string {
 	return fmt.Sprintf("Min: %.2f, Max: %.2f, Avg: %.2f", stats.min, stats.max, stats.avg)
 }
 
-func processLines(id int, start, end int64, filePath string, wg *sync.WaitGroup, results chan<- map[string]TemperatureStats) {
-	defer wg.Done()
-
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-	defer file.Close()
-
-	stats := make(map[string]TemperatureStats)
-
-	// Seek to the start position
-	_, err2 := file.Seek(start, 0)
-	if err2 != nil {
-		fmt.Printf("Worker %d - Error seeking file: %s\n", id, err)
-		return
-	}
-
-	isFirstChunk := start == 0
-	/*
-		We can't find the perfect chunk size because we don't know the exact position of the line breaks.
-		For this reason we'll have to adjust the start and end accordingly.
-	*/
-
-	// For chunks that are not the first, skip until the end of the first line break
-	if !isFirstChunk {
-		// Read bytes until you find a newline character, indicating the start of a new line
-		b := make([]byte, 1)
-		for {
-			_, err := file.Read(b)
-			if err != nil {
-				fmt.Printf("Worker %d - Error reading file: %s\n", id, err)
-				return
-			}
-
-			// fmt.Printf("Line Worker %d - read byte file: %c\n", id, b[0])
-			// Increment start position to not re-process the same byte
-			start += 1
-			if b[0] == '\n' || start >= end {
-				break
-			}
-		}
-	}
-
-	scanner := bufio.NewScanner(file)
-	currentPosition := start
-
-	// Process lines until the end of this chunk
-	for scanner.Scan() {
-		currentPosition += int64(len(scanner.Bytes())) + 1 // +1 for newline character
-		if currentPosition > end {
-			break
-		}
-		// Process the line
-		line := scanner.Text()
-		parts := strings.Split(line, ";")
-		city := parts[0]
-		temperature, err := strconv.ParseFloat(parts[1], 64)
-		if err != nil {
-			fmt.Printf("Worker %d - Error parsing flat: %s\n", id, err)
-			return
-		}
-
-		recording, exists := stats[city]
-		if !exists {
-			stats[city] = TemperatureStats{math.Inf(1), math.Inf(-1), temperature, 1, 0.0}
-		} else {
-			if temperature < recording.min {
-				recording.min = temperature
-			} else if temperature > recording.max {
-				recording.max = temperature
-			}
-			recording.sum += temperature
-			recording.count++
-
-			stats[city] = recording
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Worker %d - Error reading chunk: %s\n", id, err)
-	}
-
-	results <- stats
-}
-
+var measurementsFile = flag.String("measurements", "", "file with measurements")
 var traceFile = flag.String("trace", "", "write trace execution to `file`")
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
+var MAX_CITY_NUM = 10000
+
 func main() {
 	log.Println("Starting the application...")
 	flag.Parse()
+
+	if *measurementsFile == "" {
+		log.Fatal("Missing measurements filename")
+	}
 
 	if *traceFile != "" {
 		f, err := os.Create("./profiles/" + *traceFile)
@@ -147,7 +73,8 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	calculate()
+	calculate(*measurementsFile)
+	//calculateWithMMap(*measurementsFile)
 
 	if *memprofile != "" {
 		f, err := os.Create("./profiles/" + *memprofile)
@@ -162,9 +89,8 @@ func main() {
 	}
 }
 
-func calculate() {
-	filePath := "../../../../measurements.txt"
-	file, err := os.Open(filePath)
+func calculate(measurementsFile string) {
+	file, err := os.Open(measurementsFile)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
@@ -181,25 +107,47 @@ func calculate() {
 	numWorkers := runtime.NumCPU()
 
 	chunkSize := fileSize / int64(numWorkers)
-	results := make(chan map[string]TemperatureStats, numWorkers+1)
-	stats := make(map[string]TemperatureStats)
+	results := make(chan map[string]*TemperatureStats, numWorkers+1)
+	stats := make(map[string]*TemperatureStats, MAX_CITY_NUM)
 
 	var wg sync.WaitGroup
-	workerId := 0
-	for start := int64(0); start < fileSize; start += chunkSize {
-		end := start + chunkSize
+
+	start := int64(0)
+	end := int64(0)
+	for i := 0; i < numWorkers; i++ {
+		end = start + chunkSize
 		if end > fileSize {
 			end = fileSize
 		}
 
-		log.Printf("Adding worker %d to read file from %d up to %d \n", workerId, start, end)
+		// Read bytes until you find a newline character, indicating the start of a new line
+		b := make([]byte, 1)
+		for {
+			//fmt.Printf("Worker %d - Reading file from %d up to %d\n", i, start, end)
+			_, err := file.ReadAt(b, end-1)
+			if err != nil {
+				fmt.Printf("Worker %d - Error reading file from %d up to %d: %s\n", i+1, start, end, err)
+				return
+			}
 
+			// fmt.Printf("Line Worker %d - read byte file: %c\n", id, b[0])
+			if b[0] == '\n' || end >= fileSize {
+				//fmt.Printf("Worker %d - Found newline at %d\n", i, end)
+				break
+			}
+			end += 1
+		}
+
+		chunk := Chunk{start, end}
+
+		log.Printf("Adding worker %d to read file from %d up to %d \n", i+1, start, end)
 		wg.Add(1)
-		go processLines(workerId, start, end, filePath, &wg, results)
-		workerId++
+		//go processLines(i+1, chunk, file, &wg, results)
+		go processLinesWithScanner(i+1, chunk, measurementsFile, &wg, results)
+		start = end + 1
+
 	}
 
-	// No more tasks to be added, close the channel.
 	wg.Wait()
 	close(results)
 
@@ -231,4 +179,259 @@ func calculate() {
 	log.Println("Outputting stats...")
 
 	fmt.Print(stats)
+}
+
+func calculateWithMMap(measurementsFile string) {
+	file, err := os.Open(measurementsFile)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("Error getting file stats:", err)
+		return
+	}
+
+	fileSize := fileInfo.Size()
+	numWorkers := runtime.NumCPU()
+
+	chunkSize := fileSize / int64(numWorkers)
+	results := make(chan map[string]*TemperatureStats, numWorkers+1)
+	stats := make(map[string]*TemperatureStats, MAX_CITY_NUM)
+
+	data, err := syscall.Mmap(int(file.Fd()), 0, int(fileSize), syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		log.Fatalf("Mmap: %v", err)
+	}
+
+	defer func() {
+		if err := syscall.Munmap(data); err != nil {
+			log.Fatalf("Munmap: %v", err)
+		}
+	}()
+
+	var wg sync.WaitGroup
+
+	start := int64(0)
+	end := int64(0)
+	for i := 0; i < numWorkers; i++ {
+		end = start + chunkSize
+		if end > fileSize {
+			end = fileSize
+		}
+
+		// Read bytes until you find a newline character, indicating the start of a new line
+		for {
+			// fmt.Printf("Line Worker %d - read byte file: %c\n", id, b[0])
+			if data[end-1] == '\n' || end >= fileSize {
+				//fmt.Printf("Worker %d - Found newline at %d\n", i, end)
+				break
+			}
+			end += 1
+		}
+
+		chunk := Chunk{start, end}
+
+		log.Printf("Adding worker %d to read file from %d up to %d \n", i+1, start, end)
+		wg.Add(1)
+		go processLinesWithMMap(i+1, chunk, data, &wg, results)
+		start = end + 1
+
+	}
+
+	wg.Wait()
+	close(results)
+
+	log.Println("Merging results...")
+	for resultsMap := range results {
+		for city, recording := range resultsMap {
+			existingRecording, exists := stats[city]
+			if !exists {
+				stats[city] = recording
+			} else {
+				if recording.min < existingRecording.min {
+					existingRecording.min = recording.min
+				}
+				if recording.max > existingRecording.max {
+					existingRecording.max = recording.max
+				}
+				existingRecording.sum += recording.sum
+				existingRecording.count += recording.count
+				stats[city] = existingRecording
+			}
+		}
+	}
+
+	log.Println("Calculating averages...")
+	for city, recording := range stats {
+		recording.avg = recording.sum / float64(recording.count)
+		stats[city] = recording
+	}
+	log.Println("Outputting stats...")
+
+	fmt.Print(stats)
+}
+
+func processLines(id int, chunk Chunk, file *os.File, wg *sync.WaitGroup, results chan<- map[string]*TemperatureStats) {
+	defer wg.Done()
+
+	stats := make(map[string]*TemperatureStats, MAX_CITY_NUM)
+	b := make([]byte, chunk.end-chunk.start)
+	// Process lines until the end of this chunk
+	//r := bufio.NewReader(file)
+
+	_, err := file.ReadAt(b, chunk.start)
+	if err != nil {
+		fmt.Printf("Worker %d - Error reading file: %s\n", id, err)
+		return
+	}
+
+	offset := 0
+	for offset < len(b) {
+
+		newLinePos := bytes.IndexByte(b[offset:], '\n')
+		line := string(b[offset : offset+newLinePos])
+		//fmt.Printf("Worker %d - Processing line: %s\n", id, line)
+		// Process the line
+		parts := strings.Split(line, ";")
+		city := parts[0]
+		temperature, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			fmt.Printf("Worker %d - Error parsing flat: %s\n", id, err)
+			return
+		}
+
+		recording, exists := stats[city]
+		if !exists {
+			stats[city] = &TemperatureStats{temperature, temperature, temperature, 1, 0.0}
+		} else {
+			if temperature < recording.min {
+				recording.min = temperature
+			} else if temperature > recording.max {
+				recording.max = temperature
+			}
+			recording.sum += temperature
+			recording.count++
+
+			stats[city] = recording
+		}
+
+		offset += (newLinePos + 1)
+
+	}
+
+	results <- stats
+}
+
+func processLinesWithScanner(id int, chunk Chunk, filePath string, wg *sync.WaitGroup, results chan<- map[string]*TemperatureStats) {
+	defer wg.Done()
+
+	stats := make(map[string]*TemperatureStats, MAX_CITY_NUM)
+	// Process lines until the end of this chunk
+	//r := bufio.NewReader(file)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	currentPosition := chunk.start
+
+	// Seek to the start position
+	_, err2 := file.Seek(chunk.start, 0)
+	if err2 != nil {
+		fmt.Printf("Worker %d - Error seeking file: %s\n", id, err)
+		return
+	}
+
+	for scanner.Scan() {
+		currentPosition += int64(len(scanner.Bytes())) + 1 // +1 for newline character
+		if currentPosition > chunk.end {
+			break
+		}
+
+		line := scanner.Text()
+		//fmt.Printf("Worker %d - Processing line: %s\n", id, line)
+		// Process the line
+		parts := strings.Split(line, ";")
+		city := parts[0]
+		temperature, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			fmt.Printf("Worker %d - Error parsing flat: %s\n", id, err)
+			return
+		}
+
+		recording, exists := stats[city]
+		if !exists {
+			stats[city] = &TemperatureStats{temperature, temperature, temperature, 1, 0.0}
+		} else {
+			if temperature < recording.min {
+				recording.min = temperature
+			} else if temperature > recording.max {
+				recording.max = temperature
+			}
+			recording.sum += temperature
+			recording.count++
+
+			stats[city] = recording
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Worker %d - Error reading chunk: %s\n", id, err)
+	}
+
+	results <- stats
+}
+
+func processLinesWithMMap(id int, chunk Chunk, data []byte, wg *sync.WaitGroup, results chan<- map[string]*TemperatureStats) {
+	defer wg.Done()
+
+	stats := make(map[string]*TemperatureStats, MAX_CITY_NUM)
+	// Process lines until the end of this chunk
+	//r := bufio.NewReader(file)
+
+	b := data[chunk.start:chunk.end]
+
+	offset := 0
+	for offset < len(b) {
+
+		newLinePos := bytes.IndexByte(b[offset:], '\n')
+		line := string(b[offset : offset+newLinePos])
+		//fmt.Printf("Worker %d - Processing line: %s\n", id, line)
+		// Process the line
+		parts := strings.Split(line, ";")
+		city := parts[0]
+		temperature, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			fmt.Printf("Worker %d - Error parsing flat: %s\n", id, err)
+			return
+		}
+
+		recording, exists := stats[city]
+		if !exists {
+			stats[city] = &TemperatureStats{temperature, temperature, temperature, 1, 0.0}
+		} else {
+			if temperature < recording.min {
+				recording.min = temperature
+			} else if temperature > recording.max {
+				recording.max = temperature
+			}
+			recording.sum += temperature
+			recording.count++
+
+			stats[city] = recording
+		}
+
+		offset += (newLinePos + 1)
+
+	}
+
+	results <- stats
 }
